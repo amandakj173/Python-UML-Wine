83d37ab5f5c46904513599ba8c347f2091826096
amanda173 <amandakj173@gmail.com>
2026-06-10 14:35:15 +0100

    feat: add modelling

--- Pythin-UML-Wine.py
+++ Pythin-UML-Wine.py
@@ -5,6 +5,9 @@ import pandas as pd
 from scipy.stats import skew
 from sklearn import preprocessing
 import numpy as np
+from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
 
 # Set Parameters
 pd.set_option("display.precision", 3)
@@ -31,3 +34,31 @@ scaler.mean_  # Look at calculated means
 scaler.scale_  # Look at calculated sds
 wine_scaled = scaler.transform(data_wine)  # Apply scaling
 wine_scaled  # View tranformed array
+
+
+# MODELLING
+
+# Cluster Selection
+def gmm_bic(estimator, X):
+    return -estimator.bic(X)  # Define BIC scoring function
+
+param_grid = {
+    "n_components": range(1,10),
+    "covariance_type": ["spherical", "tied", "diag", "full"]
+}  # Set up parameter grid of 1-9 components w. all covariance types
+
+grid_search = GridSearchCV(
+    GaussianMixture(random_state = 42),  # For reproducability
+    param_grid = param_grid,
+    scoring = gmm_bic
+)  # Run grid search
+grid_search.fit(wine_scaled)  # Run grid search over scaled data
+
+best_gmm = grid_search.best_estimator_  # Label best model
+best_params = grid_search.best_params_  # Label best model parameters
+print("Best model parameters:", best_params)
+print("Best negative BIC (higher is better):", grid_search.best_score_)
+
+gmm_classification = best_gmm.predict(wine_scaled)  # Get hard cluster assignments
+
+data_wine["GMM_Cluster"] = gmm_classification  # Add cluster labels to dataframe
