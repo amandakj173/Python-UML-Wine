ef569b1cf4674ea05afc6826459e4ca1aca0543f
amanda173 <amandakj173@gmail.com>
2026-06-10 12:39:14 +0100

    feat: add preprocessing

--- Pythin-UML-Wine.py
+++ Pythin-UML-Wine.py
@@ -3,6 +3,8 @@
 # Import Packages
 import pandas as pd
 from scipy.stats import skew
+from sklearn import preprocessing
+import numpy as np
 
 # Set Parameters
 pd.set_option("display.precision", 3)
@@ -22,3 +24,10 @@ skewness_wine = skew(data_wine)
 print(skewness_wine)
 
 
+# PREPROCESSING
+scaler = preprocessing.StandardScaler().fit(data_wine)  # Create scaler object & fit to data (find mean & sd)
+scaler  # Print representation of object
+scaler.mean_  # Look at calculated means
+scaler.scale_  # Look at calculated sds
+wine_scaled = scaler.transform(data_wine)  # Apply scaling
+wine_scaled  # View tranformed array
