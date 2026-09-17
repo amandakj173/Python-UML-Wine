7a6a3317099290ad904bc33393730571e38a765a
amanda173 <amandakj173@gmail.com>
2026-09-17 12:48:16 +0100

    build: data preprocessing - add outlier detection & removal to reduce potential effects during analysis

--- Pythin-UML-Wine.py
+++ Pythin-UML-Wine.py
@@ -2,12 +2,16 @@
 
 # Import Packages
 import pandas as pd
+from pandas.core.methods import describe
 from scipy.stats import skew
-from sklearn import preprocessing
-import numpy as np
+from scipy import stats
 from sklearn.mixture import GaussianMixture
 import matplotlib.pyplot as plt
 from sklearn.model_selection import GridSearchCV
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+import numpy as np
 
 # Set Parameters
 pd.set_option("display.precision", 3)
@@ -15,25 +19,31 @@ pd.set_option("display.max_columns", 20)
 pd.set_option("display.width", 2000)
 
 # Import Data
-data_wine = pd.read_csv("wine.csv")
+ds = pd.read_csv("wine.csv")
 
-# Initial Data Exploration
-print(data_wine)
-data_wine.info()
+# Basic Data Exploration
+print(ds)
+ds.info()
 
 # Advanced Data Exploration
-print(data_wine.describe())
-skewness_wine = skew(data_wine)
-print(skewness_wine)
+print(ds.describe())
+print(skew(ds))
 
 
 # PREPROCESSING
-scaler = preprocessing.StandardScaler().fit(data_wine)  # Create scaler object & fit to data (find mean & sd)
-scaler  # Print representation of object
-scaler.mean_  # Look at calculated means
-scaler.scale_  # Look at calculated sds
-wine_scaled = scaler.transform(data_wine)  # Apply scaling
-wine_scaled  # View tranformed array
+# Identify & remove outliers
+z = np.abs(stats.zscore(ds))
+print(z)
+
+threshold_z = 3
+outlier_indices = np.where(z > threshold_z)[0]
+no_outlier_ds = ds.drop(outlier_indices)
+print(no_outlier_ds)
+
+# Scaling
+scale = StandardScaler()
+scaled_ds = scale.fit_transform(no_outlier_ds)
+print(scaled_ds)
 
 
 # MODELLING
@@ -62,3 +72,14 @@ print("Best negative BIC (higher is better):", grid_search.best_score_)
 gmm_classification = best_gmm.predict(wine_scaled)  # Get hard cluster assignments
 
 data_wine["GMM_Cluster"] = gmm_classification  # Add cluster labels to dataframe
+
+# VISUALISATION
+
+# Visualisation
+cluster_counts = pd.Series(gmm_classification).value_counts().sort_index()
+GMMCluster = pd.DataFrame({
+    "Cluster": cluster_counts.index,
+    "Size": cluster_counts.values
+})
+print("\nCluster sizes (GMM):", GMMCluster)
+print(GMMCluster)  # Cluster size table
