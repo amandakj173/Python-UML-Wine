2ee8b4c8ff3fd756da9bfcf0c79d0512bc83fdf3
amanda173 <amandakj173@gmail.com>
2026-09-17 14:17:39 +0100

    build: check clustering feature profiles -
     Cluster one is characterised by very low levels of alcohol, colour intensity, and proline, and slightly heightened hue.
    
     Cluster two is characterised by high malic acid, non-flavanoid phenols and colour intensity, and low magnesium, total phenols, proanthocynanins and hue.
    
     Cluster three scores highly across most clusters, being characterised by high alcohol, magnesium, total phenols, flavenoids, proanthocynanins, hue, and proline, and low non-flavanoid phenols.

--- Python-UML-Wine.py
+++ Python-UML-Wine.py
@@ -70,8 +70,13 @@ print("Best negative BIC:", grid_search.best_score_)
 # Clustering
 scaled_ds["GMM_Cluster"] = best_gmm.predict(scaled_ds)
 
+
 # CHECK CLUSTERING
 
 # Check balances & sizes
 print(scaled_ds["GMM_Cluster"].value_counts())
 print(scaled_ds["GMM_Cluster"].value_counts(normalize=True) * 100)
+
+# Feature Profiles
+cluster_means = scaled_ds.groupby("GMM_Cluster").mean()
+print(cluster_means)
